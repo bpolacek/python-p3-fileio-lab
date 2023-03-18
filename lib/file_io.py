0e9f049504b7ce70e53341182e00307e221db7cc
Brett Polacek <brettpolacek@gmail.com>
2023-03-18 14:02:26 -0400

    files updated

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
+
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
+
+def right_file(file_name, file_content):
+    with open(f"{file_name}.txt", "w") as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(append_content)
+
+def appoond_file(file_name, appoond_content):
+    with open(f"{file_name}.txt", "a") as f:
+        file.write(appoond_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt") as file:
+        return file.read()
